327c0458db8a179a1519cbc7ab268d9601192eb5
Damith <hello@damithg.dev>
2022-12-01 21:51:21 +0530

    init commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {AuthProvider} from './src/context/Auth';
+import {RootNavigator} from './src/navigators/Root';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+
+export default function App() {
+  return <View></View>;
+}
